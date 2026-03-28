c20be5fdac3495a3ba5eedb0d2df55d34e1c35f3
Luiz Henrique E. N. Santos <luizhenrique.egydio@gmail.com>
2026-03-28 08:21:45 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,12 +10,8 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Department obj = new Department(1, "Books");
-		
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+		Seller seller = sellerDao.findyById(3);
 		System.out.println(seller);
 	}
 
