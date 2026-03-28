d1a947f33740a4b368ab4eac286ecee04a3e920a
Luiz Henrique E. N. Santos <luizhenrique.egydio@gmail.com>
2026-03-28 14:54:36 -0300

    findAll implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -90,8 +90,40 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement(
+					"select seller.*, department.Name as DepName "
+					+ "from seller inner join department "
+					+ "on seller.departmentId = department.Id "
+					+ "order by Name");
+			rs = st.executeQuery();
+			
+			List<Seller> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();
+			
+			while(rs.next()) {
+				
+				Integer mapKeyValue = rs.getInt("DepartmentId");
+				Department dep = map.get(mapKeyValue);
+				
+				if(dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(mapKeyValue, dep);
+				}
+				
+				Seller seller = intantiateSeller(rs, dep);
+				list.add(seller);
+			}
+			return list;
+		}catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);;
+			DB.closeResultSet(rs);
+		}
 	}
 
 	@Override
